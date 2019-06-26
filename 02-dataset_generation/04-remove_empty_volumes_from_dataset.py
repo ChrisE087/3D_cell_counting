70d384dafc7b8db830414cf9daa7303d75087f93
Chris <1813254@stud.hs-mannheim.de>
2019-06-26 22:17:38 +0200

    -

--- 02-dataset_generation/04-remove_empty_volumes_from_dataset.py
+++ 02-dataset_generation/04-remove_empty_volumes_from_dataset.py
@@ -25,9 +25,9 @@ for filename in os.listdir(path_to_dataset):
         print('Cannot open ', filename, ' permission error')
     # Check if the file exclusively consists of padding (sum over voxels must 
     # be greater than threshold)
-    threshold = 0.00001    # Number of cells in patch
+    threshold = 0.0#0.00001    # Number of cells in patch
     cell_num = np.sum(data[1,])
-    if cell_num < threshold:
+    if cell_num <= threshold:
         print('Moving file ', filename, ' cell number: ', cell_num)
         shutil.move(filepath, path_to_padding_set)
         
