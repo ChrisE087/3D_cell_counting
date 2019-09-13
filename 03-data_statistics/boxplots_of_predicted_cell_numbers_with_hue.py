64d3ef6d73c595723ba6eeabc102cab76d89aa9c
Chris <1813254@stud.hs-mannheim.de>
2019-09-13 15:48:01 +0200

    -

--- 03-data_statistics/boxplots_of_predicted_cell_numbers_with_hue.py
+++ 03-data_statistics/boxplots_of_predicted_cell_numbers_with_hue.py
@@ -5,7 +5,7 @@ import os
 
 # Load a table of data
 path_to_table1 = os.path.join('..', '..', '..', 'Notizen', 'Analyse Zellanzahl Vergleich der Trainings', 'Vorhersagen durch Segmentierung', 'box_plot.csv')
-path_to_table1 = os.path.join('box_plot.csv')
+path_to_table1 = os.path.join('Anhang_F.csv')
 predictions1 = pd.read_csv(path_to_table1, sep=';')
 
 # Make a Boxplot
@@ -13,8 +13,9 @@ fig = plt.figure()
 fig.autolayout : True
 plt.tight_layout()
 #ax = sns.boxplot(x=predictions1['rel_diff'], y=predictions1['T'], hue=predictions1['Net'])
-ax = sns.boxplot(x=predictions1['rel_diff'], y=predictions1['Typ'])
-ax.set(xlabel='Relative Abweichung der vorhergesagten Zellkernanzahl von der Ground-Truth', ylabel='Zelltyp')
+ax = sns.boxplot(x=predictions1['Relative Differenz'], y=predictions1['Kultivierungszeitraum'], hue=predictions1['Netz'])
+#ax.set(xlabel='Relative Abweichung der Kolokalisation mittels vorhergesagter Segmentierung \nvon der Kolokalisation mit der Ground-Truth-Segmentierung in %', ylabel='Kultivierungszeitraum')
+ax.set(xlabel='Relative Abweichung der vorhergesagten Zellkernanzahl von der Ground-Truth in %', ylabel='Kultivierungszeitraum')
 plt.savefig('box_plot.svg', format='svg', bbox_inches='tight')
 
 # Make a Boxplot
